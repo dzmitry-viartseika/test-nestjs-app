80d7eccdae12a7e7a0e311e40f47e7df70240232
Dzmitry Viartseika <verteyko1990@gmail.com>
2024-09-13 22:51:12 +0200

    add class-validator

---
+++ src/config/app.config.ts
@@ -0,0 +1,22 @@
+import { isEnum, IsString } from 'class-validator';
+import { Environment } from './types/enums/configurations.enums';
+
+export class ApplicationConfig {
+  @isEnum(Environment, {always: true})
+  NODE_ENV: Environment;
+
+  @IsString({always: true})
+  SERVICE_NAME: string;
+
+  @IsString({always: true})
+  HTTP_HOST: string;
+
+  @IsString({always: true})
+  HTTP_PORT: string;
+
+  @IsString({always: true})
+  HTTP_PREFIX: string;
+
+  @IsString({always: true})
+  HTTP_VERSION: string;
+}
