939581349d2a32a61b28dba9b347b43681d9447b
Dzmitry Viartseika <verteyko1990@gmail.com>
2024-09-26 21:07:52 +0200

    add auth service

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { UserModule } from './module/user/user.module';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './module/database/database.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UserModule, ConfigModule.forRoot(), DatabaseModule],
+  imports: [UserModule, ConfigModule.forRoot(), DatabaseModule, AuthModule],
 })
 export class AppModule {}
